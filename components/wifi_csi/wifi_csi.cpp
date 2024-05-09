987791b6f42e86a6ad1596632ab0a4ad5744c082
Galcohen93 <galcoh93@gmail.com>
2024-05-09 11:28:29 +0200

    added delay 5 sec

--- components/wifi_csi/wifi_csi.cpp
+++ components/wifi_csi/wifi_csi.cpp
@@ -71,6 +71,8 @@ void esphome::wifi_csi::CsiSensor::update() {
     static float sum = 0.0;   // sum of all rssi values
 
     if (m_rssi) {            
+        vTaskDelay(pdMS_TO_TICKS(5000));
+        ESP_LOGD(TAG, "delay: %d", 1);
         int currentRssi = 0;
         if (nullptr != esphome::wifi::global_wifi_component) currentRssi = esphome::wifi::global_wifi_component->wifi_rssi();
         if (cnt == m_bufferSize) {
