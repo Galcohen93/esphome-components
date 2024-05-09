a26e3c7f94af0ec3241d9af2f5a3f13aff65da77
Galcohen93 <galcoh93@gmail.com>
2024-05-09 15:09:22 +0200

    push to test

--- components/wifi_csi/wifi_csi.cpp
+++ components/wifi_csi/wifi_csi.cpp
@@ -65,24 +65,24 @@ void esphome::wifi_csi::CsiSensor::set_buffer_size(int bufferSize)
     m_rssi = reinterpret_cast<int*>(malloc(m_bufferSize * sizeof(int)));
 }
 
-void esphome::wifi_csi::CsiSensor::set_buffer_vacant(int bufferSize){
-    int sum = 0;
-    int mean = 0;
-    double standard_dev = 0;
-    for(int indx = 0; indx < bufferSize;indx++){
-        int currentRssi = 0;
-        if (nullptr != esphome::wifi::global_wifi_component) currentRssi = esphome::wifi::global_wifi_component->wifi_rssi();
-            m_rssi[indx]  = currentRssi;
-            sum +=  currentRssi;
-    }
-    mean  = sum / bufferSize;
-        for(int indx = 0;indx < bufferSize;indx++){
-            standard_dev += pow(m_rssi[indx]-mean , 2);
-    } 
-    standard_dev =sqrt(standard_dev/bufferSize) ;
-    set_sensitivity(standard_dev);
+// void esphome::wifi_csi::CsiSensor::set_buffer_vacant(int bufferSize){
+//     int sum = 0;
+//     int mean = 0;
+//     double standard_dev = 0;
+//     for(int indx = 0; indx < bufferSize;indx++){
+//         int currentRssi = 0;
+//         if (nullptr != esphome::wifi::global_wifi_component) currentRssi = esphome::wifi::global_wifi_component->wifi_rssi();
+//             m_rssi[indx]  = currentRssi;
+//             sum +=  currentRssi;
+//     }
+//     mean  = sum / bufferSize;
+//         for(int indx = 0;indx < bufferSize;indx++){
+//             standard_dev += pow(m_rssi[indx]-mean , 2);
+//     } 
+//     standard_dev =sqrt(standard_dev/bufferSize) ;
+//     set_sensitivity(standard_dev);
 
-}
+// }
 
 
 void esphome::wifi_csi::CsiSensor::update() {
@@ -118,7 +118,7 @@ void esphome::wifi_csi::CsiSensor::update() {
             last_t = now_t;
         }
     } else {
-        // ESP_LOGC(TAG, "Wait for %d minuete and %d seconds:", 1, 10);
+        // ESP_LOGD(TAG, "Wait for %d minuete and %d seconds:", 1, 10);
         // vTaskDelay(pdMS_TO_TICKS(60000));
         set_buffer_size(m_bufferSize);
         // set_buffer_vacant(m_bufferSize); // create the rssi buffer
